416b53e85149cd7a7c6d179c8d7f5aa1e0eb7287
Unknown <jvelasquezm@psl.com.co>
2019-02-12 07:28:54 -0500

    Param for results directory

--- handler.py
+++ handler.py
@@ -2,12 +2,7 @@ import os
 import sys
 import time
 
-
-#python <pythonFile> <binJmeter> <paramsFile> <jmxFile> <script.sh>
-#python /home/richar_amador/performance/assessmentServices/scripts/handler.py /home/richar_amador/performance/apache-jmeter-4.0/bin/jmeter /home/richar_amador/performance/assessmentServices/datasets/params.txt /home/richar_amador/performance/assessmentServices/scripts/getAssessmentDetail.jmx /home/richar_amador/performance/assessmentServices/scripts/jmeterResults.sh
-
 paramsLoaded = []
-
 
 def main():
     if not paramsOk(sys.argv):
@@ -16,16 +11,14 @@ def main():
     loadTestParams(paramsFile)
     startTestSuite()
 
-
 def paramsOk(params):
-    if len(params) < 5:
-        print("Too few params, 4 expected " + str(len(params)-1) + " given")
+    if len(params) < 6:
+        print("Too few params, 5 expected " + str(len(params)-1) + " given")
         return False
-    elif len(params) > 5:
-        print("Too many params, 4 expected. " + str(len(params)-1) + " given")
+    elif len(params) > 6:
+        print("Too many params, 5 expected. " + str(len(params)-1) + " given")
         return False
     return True
-
 
 def loadTestParams(paramsFile):
     with open(paramsFile,"r") as f:
@@ -36,17 +29,18 @@ def loadTestParams(paramsFile):
                 paramsLoaded.append(line.split(","))
             line = f.readline()
 
-
-def createResultsDir(concurrency, duration):
+def createResultsDir(concurrency, duration, jmxFile):
+    pathName = jmxFile.split("/")
+    fileName = pathName[-1].split(".")
     userHome = os.getenv("HOME")
-    pathToDesktop = userHome + "/performance/Results/"
+    resultsPath = sys.argv[5]
+    pathToDesktop = userHome + resultsPath
     date = time.ctime(time.time())
     formatedDate = date.replace(" ", "_").replace(":", ".")
-    folderName = "%s%s_c%s_d%s_%s" % (pathToDesktop, "Test", concurrency,
+    folderName = "%s%s_c%s_d%s_%s" % (pathToDesktop, fileName[0], concurrency,
         duration,formatedDate)
     os.mkdir(folderName)
     return folderName
-
 
 def startTestSuite():
     jmeterBin = sys.argv[1]
@@ -54,12 +48,11 @@ def startTestSuite():
     batFile = sys.argv[4] + " "
     for keyVal in paramsLoaded:
         concurrency, duration = keyVal
-        dirName = createResultsDir(concurrency, duration)
+        dirName = createResultsDir(concurrency, duration, JMXFile)
         ResultFile = dirName + "/result"    
         command = "%s %s %s %s %s" % (batFile, concurrency, duration,
             ResultFile, JMXFile) 
         p = os.system(command)
-        p.read()  
         msg = "Test for Jthread=%s and Jduration=%s ... done" % (concurrency,
             duration)
         print(msg)
--- jmeterTest.sh
+++ jmeterTest.sh
@@ -5,6 +5,6 @@ duration=$2
 resultFile=$3
 jmxPath=$4
 
-/home/richar_amador/performance/apache-jmeter-4.0/bin/jmeter -Jthreads=${concurrency} -Jduration=${duration} -n -t ${jmxPath} -l ${resultFile}.jtl >> ${resultFile}.log
+${jmeterRoute}/jmeter -Jthreads=${concurrency} -Jduration=${duration} -n -t ${jmxPath} -l ${resultFile}.jtl >> ${resultFile}.log
 
-/home/richar_amador/performance/apache-jmeter-4.0/bin/JMeterPluginsCMD.sh --generate-csv ${resultFile}.csv --input-jtl ${resultFile}.jtl --plugin-type AggregateReport
+${cmdRoute}/JMeterPluginsCMD.sh --generate-csv ${resultFile}.csv --input-jtl ${resultFile}.jtl --plugin-type AggregateReport
