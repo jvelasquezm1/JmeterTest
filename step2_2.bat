2f1d45922a8b25b001450fa2116defe3c9fae3d4
Juanes Velasquez <jvelasquezm@psl.com.co>
2019-04-30 07:26:41 -0500

    add scripts to run it in windows

---
+++ step2_2.bat
@@ -0,0 +1,8 @@
+
+MKDIR %dirBase%%data%
+MKDIR %dirBase%%results%
+MKDIR %dirBase%%jmeterScripts%
+MKDIR %dirBase%%executionSuite%
+
+SETX   DATA_JSCRIPTS %dirBase%%data%
+SETX   RESULT_JSCRIPTS %dirBase%%results%
