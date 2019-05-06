1c1cd955fe85d0b2a659f01c1dc213685296d23f
Juanes Velasquez <jvelasquezm@psl.com.co>
2019-05-06 13:55:03 -0500

    update startPython to receive xlsx

--- startPython.bat
+++ startPython.bat
@@ -9,18 +9,15 @@ REM Path of jmeter.bat
 set jmeterPath=%jmeterExePath%
 REM Path of scripts of jmeter to execute (.jmx files)
 set jmeterScripts=%jmeterScripts%
-REM Name of script of jmeter to execute (.jmx files)
-set scriptName=createAssessmentSchedule.jmx
 REM Path to save the result of cycle executions
 set executionResultsPath=%results%
 
 REM Python parameters
 set handlerPython=%workSpace%%executionSuite%handler4.py
 set jmeter=%jmeterPath%
-set params=%workSpace%%executionSuite%params.txt
-set jmxFile=%workSpace%%jmeterScripts%%scriptName%
+set params=%workSpace%%executionSuite%Params.xlsx
 set jmetersh=%workSpace%%executionSuite%jmeterResults2.bat
 set resultsFolder=%workSpace%%executionResultsPath%
 
 REM Execute python command
-python %handlerPython% %jmeter% %params% %jmxFile% %jmetersh% %resultsFolder% %scriptName%
+python %handlerPython% %jmeter% %params% %jmetersh% %resultsFolder%
