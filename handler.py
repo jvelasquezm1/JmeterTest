e4103b020a494c95a7b9039bc7341e61bf81c0e0
Juanes Velasquez <jvelasquezm@psl.com.co>
2019-05-06 13:54:21 -0500

    update handler to receive xlsx

--- handler.py
+++ handler.py
@@ -1,44 +1,37 @@
 import os
 import sys
 import time
+import pandas as pd
+import numpy
 
 #python <pythonFile> <binJmeter> <paramsFile> <jmxFile> <script.bat>
-
-#Loading python paramneters
-paramsLoaded = []
 
 #Reading concurrence, duration and ramp up paramneters from params.txt
 def main():
     if not paramsOk(sys.argv):
         sys.exit()
     paramsFile = sys.argv[2]
-    loadTestParams(paramsFile)
-    startTestSuite()
+    paramsLoaded = loadTestParams(paramsFile)
+    startTestSuite(paramsLoaded)
 
 #Verifying that the number of parameter match with the expected 
 def paramsOk(params):
-    if len(params) < 7:
-        print("Too few params, 6 expected " + str(len(params)-1) + " given")
+    if len(params) < 5:
+        print("Too few params, 4 expected " + str(len(params)-1) + " given")
         return False
-    elif len(params) > 7:
-        print("Too many params, 6 expected. " + str(len(params)-1) + " given")
+    elif len(params) > 5:
+        print("Too many params, 4 expected. " + str(len(params)-1) + " given")
         return False
     return True
 
 #Setting number of execution of the cycle
 def loadTestParams(paramsFile):
-    with open(paramsFile,"r") as f:
-        line = f.readline()
-        while(line):
-            line = line.strip("\n")
-            if line and not line.startswith("#") :
-                paramsLoaded.append(line.split(","))
-            line = f.readline()
+    df = pd.read_excel(paramsFile, sheetname='Sheet1')
+    return df
 
 #Creating bthe folders to save the each execution results
-def createResultsDir():
-    resultsPath = sys.argv[5]
-    scriptName = sys.argv[6]
+def createResultsDir(scriptName):
+    resultsPath = sys.argv[4]
     date = time.ctime(time.time())
     formatedDate = date.replace(" ", "_").replace(":", ".")
     folderName = "%s%s%s" % (resultsPath, formatedDate, scriptName)
@@ -46,22 +39,24 @@ def createResultsDir():
     return folderName
 
 # Creatring parameters to jmeterResults script (jmeterResults2.bat)	
-def startTestSuite():
+def startTestSuite(paramsLoaded):
 	
     jmeterBin = sys.argv[1]
-    JMXFile = sys.argv[3]
-    batFile = sys.argv[4] + " "
-    pathName = JMXFile.split("\\") 
-	    
-    fileName = pathName[-1].split(".")
-    dirName = createResultsDir()
-    for keyVal in paramsLoaded:
-        concurrency, duration, rampUpPeriod = keyVal
-        ResultFile = dirName + "\\" + fileName[0] + "_c" + concurrency + "_d" + duration
-        ResultFileDash = dirName + "\\" + concurrency + "\\"
-        command = "%s %s %s %s %s %s %s" % (batFile, concurrency, duration,ResultFile, JMXFile, rampUpPeriod, ResultFileDash) 
-        p = os.system(command)
-        msg = "Test for Jthread=%s and Jduration=%s ... done" % (concurrency,duration)
-        print(msg)
+    batFile = sys.argv[3] + " "
+    for index, row in paramsLoaded.iterrows():
+        if(row['Total Duration'] == row['Total Duration']):
+            duration = str(row['Total Duration'])
+            concurrency = str(row['Concurrence Users'])
+            rampUpPeriod = str(row['Ramp-Up'])
+            JMXFile = str(row['Jmeter Script'])
+            dirName = createResultsDir(JMXFile)
+            pathName = JMXFile.split("\\") 
+            fileName = pathName[-1].split(".")            
+            ResultFile = dirName + "\\" + fileName[0] + "_c" + concurrency + "_d" + duration
+            ResultFileDash = dirName + "\\" + concurrency + "\\"
+            command = "%s %s %s %s %s %s %s" % (batFile, concurrency, duration,ResultFile, JMXFile, rampUpPeriod, ResultFileDash) 
+            p = os.system(command)
+            msg = "Test for Jthread=%s and Jduration=%s ... done" % (concurrency,duration)
+            print(msg)
 
 main()
